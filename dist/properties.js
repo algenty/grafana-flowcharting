c27252429b406c780c52154e7dd50905e2051733
Arnaud GENTY <arnaud.genty@gmail.com>
2019-01-26 13:41:12 +0100

    INIT Commit for backup

---
+++ dist/properties.js
@@ -0,0 +1,14 @@
+
+var pluginName = 'agenty-flowcharting-panel',
+	flowchartEditor = 'public/plugins/'+ pluginName +'/flowchartEditor.html',
+	displayEditor = 'public/plugins/'+ pluginName +'/displayEditor.html',
+	shapeEditor = 'public/plugins/'+ pluginName +'/shapeEditor.html';
+	valueEditor = 'public/plugins/'+ pluginName +'/valueEditor.html';
+
+export {
+	pluginName,
+	flowchartEditor,
+	displayEditor,
+	shapeEditor,
+	valueEditor
+}
