de2414b9286cf7fee365ab762519bff49f22c1bc
Arnaud GENTY <arnaud.genty@gmail.com>
2019-11-03 20:33:05 +0100

    init docs

--- spec/globalTeardown.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line func-names
-module.exports = function () {
-  __express__.close();
-};
--- src/module.js
+++
@@ -1,9 +0,0 @@
-import { loadPluginCss } from 'app/plugins/sdk';
-import { FlowchartCtrl } from './flowchart_ctrl';
-
-loadPluginCss({
-  dark: 'plugins/agenty-flowcharting-panel/css/flowchart.dark.css',
-  light: 'plugins/agenty-flowcharting-panel/css/flowchart.light.css',
-});
-
-export { FlowchartCtrl as PanelCtrl };
