3639865b379e619282e505225e00380864e333a5
Arnaud GENTY <dosy07@gmail.com>
2019-11-26 07:59:00 +0100

    backup

--- src/metricHandler.ts
+++ src/metricHandler.ts
@@ -7,40 +7,47 @@ declare var GFP: FlowChartingPlugin;
 export default class MetricHandler {
   panel: any;
   $scope: ng.IScope;
-  table: Table[] = [];
-  serie: Serie[] = []
-  metric: Metric 
+  tables: Table[] = [];
+  series: Serie[] = []
+  metrics: (Serie|Table)[] = [];
   constructor($scope: ng.IScope, ctrl: any) {
     this.panel = ctrl.panel;
     this.$scope = $scope;
   }
 
   onDataReceived(dataList: any) {
+    this.tables = [];
+    this.series = [];
+    this.metrics = [];
     const data: any = {
       scopedVars: _.extend({}, this.panel.scopedVars),
     };
 
     dataList.array.forEach( (dl) => {
-      let met;
-      if (dl.type === 'table') {
-        dl.dataType = 'table';
-        let met = new Table(dl);
-        const tableData = dataList.map(this.tableHandler.bind(this));
-        this.setTableValues(tableData, data);
-      } else {
-
-      }
+      this.addMetric(dl);
     });
-    this.data = data;
   }
 
-  addTable(data:any) {
+  addMetric(data:any) {
+    if (data.type === 'table') {
+      this.addTable(data)
+    } else {
+      this.addMetric(data);
+    }
+  }
+  
+  addTable(data:any):Table {
     let table = new Table(data);
-    
+    this.tables.push(table);
+    this.metrics.push(table);
+    return table;
   }
 
-  addSerie(data:any) {
-
+  addSerie(data:any):Serie {
+    let serie = new Serie(data);
+    this.series.push(serie);
+    this.metrics.push(serie);
+    return serie;
   }
 
 }
