74a1d58bbc74ac82e2ba9c71c3c7c18ad574558a
Arnaud GENTY <dosy07@gmail.com>
2019-12-17 23:09:41 +0100

    backup

--- config/jest-setup.js
+++ config/jest-setup.js
@@ -16,9 +16,6 @@ global.moment = moment;
 import angular from "angular";
 global.angular = angular;
 
-import GF_PLUGIN from "../src/plugin";
-GF_PLUGIN.dirname = '/public/plugins/agenty-flowcharting-panel/';
-GF_PLUGIN.mxBasePath = `${GF_PLUGIN.dirname}libs/mxgraph/javascript/dist/`;
-GF_PLUGIN.mxImagePath = `${GF_PLUGIN.mxBasePath}images/`;
-GF_PLUGIN.partialPath = `${GF_PLUGIN.dirname}/partials/`;
-global.GF_PLUGIN = GF_PLUGIN;
+import FlowChartingPlugin from "../src/plugin";
+GFP = FlowChartingPlugin.init($scope,templateSrv);
+global.GF_PLUGIN = GFP;
