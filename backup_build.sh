1167ae8d8771d86984dafdb12cf8b74befbd1969
Arnaud <arnaud@DESKTOP-8LSJPIK.localdomain>
2020-04-29 23:32:47 +0200

    backup

--- backup_build.sh
+++
@@ -1,8 +0,0 @@
-CUR_PWD=$(pwd)
-DATE_SAVE=$(date "+%Y-%m-%d_%H%M%S")
-cd src
-for f in $(ls -1 *.ts)
-do
-  echo $f
-  cp $f ../backups/${f}_${DATE_SAVE}
-done
--- restore_ts.sh
+++
@@ -1,9 +0,0 @@
-find ./src -name '*.ts' -empty |while read file
-do
-  echo "$file is empty"
-  base=$(basename $file)
-  backup=$(ls -1t ./backups/${base}*|head -1)
-  set -x
-  cp $backup $file
-  set +x
-done
