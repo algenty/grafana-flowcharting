cbd2134d41a0d58156c7f2d52edb8c3a962bac93
Arnaud <arnaud@DESKTOP-8LSJPIK.localdomain>
2020-11-09 22:28:31 +0100

    backup

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,7 +5,7 @@ type mxMouseEvent = any;
 type mxGeometry = { x: number; y: number; width: number; height: number };
 
 declare module gf {
-  interface TSelectString {
+  declare interface TSelectString {
     text: string;
     value: string;
   }
