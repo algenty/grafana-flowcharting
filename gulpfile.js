06645cb003c58b0f6208160bb29c70b2d6d66870
Arnaud GENTY <dosy07@gmail.com>
2022-04-29 16:02:54 +0200

    dawio decode => KO gulp clean check package => OK Add editorconfig

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 // Imports
-var {src, dest, series} = require('gulp');
-var clean = require('gulp-clean');
-var zip = require('gulp-zip');
+const { src, dest, series, done } = require('gulp');
+const del = require('del');
+const zip = require('gulp-zip');
+const utils = require('fancy-log');
+const { exec } = require('child_process');
 
 // Variables
 const plugin_file = './src/plugin.json';
@@ -12,54 +14,65 @@ const package_file = './package.json'
 const package_json = require(package_file);
 const package_version = package_json.version;
 const zip_matches = ['!key.txt', '**/*', '!src/**', '!node_modules/**', '!backups/**', '!others/**', '!.git/**', '!archives/**', '!public/**', '!backup/**', '!spec/**', '!spec/__snapshots__/**'];
-const zip_filename = `${plugin_name}-${plugin_version}-SNAPSHOT.zip` 
+const zip_filename = `${plugin_name}-${plugin_version}-SNAPSHOT.zip`
 const zip_path = process.env.FLOWCHARTING_BUILT_PATH
 const require_env = ['FLOWCHARTING_PLUGIN_HOME', 'FLOWCHARTING_BUILT_PATH',]
 
 // Private functions
 function _is_Set(envName) {
-  var env = process.env["envName"]
-  if(env === null || env === undefined || env.length === 0) {
-    return false 
+  // console.log("envName",envName);
+  // console.log("process.env[envName]",process.env[envName]);
+  var env = process.env[envName];
+  if (env === null || env === undefined || env.length === 0) {
+    return false;
   }
+  return true;
 }
 
 // Tasks
 function clean() {
-  return src('./dist/').pipe(clean())
+  return del('./dist/**/*');
 }
 
 function package() {
-  console.log("Filename : " + zip_filename)
-  console.log("Destination : " + zip_path)
-  if(! _is_Set("FLOWCHARTING_BUILT_PATH") ) {
-    throw new Error("variable FLOWCHARTING_BUILT_PATH not set")
+  utils.info(`File name : '${zip_filename}'`);
+  utils.info(`Path destination : '${zip_path}'`);
+  if (!_is_Set("FLOWCHARTING_BUILT_PATH")) {
+    utils.error("variable 'FLOWCHARTING_BUILT_PATH' not set")
   }
-  return src(zip_matches).pipe(zip(zip_filename)).pipe(dest(zip_path))
+  return src(zip_matches).pipe(zip(zip_filename)).pipe(dest(zip_path));
 }
 
-function checkVars() {
-  require_env.map((envName) => {
-    if (! _is_Set(envName)) {
-      let message = `variable ${envName} not set`;
-      throw new Error(message)
-    }
-  });
+function check_vars() {
+  return Promise.all(
+    require_env.map(
+      async (envName) => {
+        if (!_is_Set(envName)) {
+          utils.error(`variable ${envName} not set`);
+          reject();
+        }
+        utils.info(`${envName} : ${process.env[envName]}`);
+        return process.env[envName];
+      }
+    )
+  )
 }
 
-function checkVersion() {
-  if( plugin_json === package_version ) {
-    let message = `properties version in ${plugin_file} doen't equal version in ${package_file}`;
-    console.log(message)
-    throw new Error(message)
-    return false;
+function build() {
+  return exec('npx grafana-toolkit plugin:build')
+}
+
+async function check_version() {
+  if (plugin_version === package_version) {
+    utils.info(`${plugin_file}.version : ${plugin_version}`)
+    return plugin_version;
   }
-  done();
+  utils.error(`properties version in ${plugin_file} not the same in ${package_file}`);
+  reject();
 }
 
 module.exports = {
   clean,
   package,
-  checkVars,
-  checkVersion,
+  check : series([check_vars, check_version])
 }
