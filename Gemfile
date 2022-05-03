db55e0a7c8ab7a9e2173194df2edc1330f9906cd
Arnaud GENTY <dosy07@gmail.com>
2022-05-03 16:44:23 +0200

    build docker works

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ source "https://rubygems.org"
 # to publish on github page
 gem 'github-pages', group: :jekyll_plugins
 
+group :jekyll_plugins do
+  gem "webrick"
+end
+
 # to publich without github page
 #gem "jekyll"
-
-gem "webrick", "~> 1.7"
--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 # source : https://github.com/envygeeks/jekyll-docker/blob/master/README.md
 
-echo "$0 [port]"
+[ -z ${FLOWCHARTING_DOCS_HOME+x} ] && echo "Variable WORKSPACE_HOME not set" && exit 1
+[ -z ${FLOWCHARTING_DOCS_HOME+d} ] && echo "Directory ${FLOWCHARTING_DOCS_HOME} not exist or permission denied" && exit 1
 
-export _JEKYLL_VERSION="${JEKYLL_VERSION:-4.0}"
-export _JEKYLL_PORT=${1:-4000}
+_JEKYLL_VERSION="${JEKYLL_VERSION:-4.0}"
 
 docker run --rm \
   -it --ipc=host\
   --dns 8.8.8.8 \
-  --publish ${_JEKYLL_PORT}:${_JEKYLL_PORT} \
   --volume="${PWD}:/srv/jekyll:Z" \
   --volume="${PWD}/vendor:/usr/local/bundle:Z" \
   jekyll/jekyll:${_JEKYLL_VERSION} \
-  jekyll serve --port ${_JEKYLL_PORT} --watch
+  jekyll build
